d42ce422632df5c507bde5bd798654e93cf0aec4
David <redpandabreeze@gmail.com>
2024-09-26 11:22:39 +0200

    Fixed timer and countdown

--- main.gd
+++ main.gd
@@ -5,50 +5,53 @@ extends Node2D
 @export var cpu: Node2D
 @export var player_score_label: Label
 @export var cpu_score_label: Label
-@export var reset_time: float = 3.0
-@onready var reset_timer_label: Label = $HUD/Reset_timer
+@onready var reset_timer_label: Label = %Reset_timer
+@onready var reset_fade_background: ColorRect = %Reset_fade_background
+
+@onready var timer: Timer = $Timer
+
+
 
 const  CENTER = Vector2(640, 450)
 var player_score: int = 0
 var cpu_score: int = 0
 
 func _ready() -> void:
-	reset_timer_label.hide()
-
+	pass
+	reset_fade_background.hide()
+	
+	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		get_tree().paused = true
 		print("pause")
+	var time_left: int  = int(timer.time_left)
+	reset_timer_label.text = str(time_left + 1)
 
 
 func _on_area_left_body_entered(body: Node2D) -> void:
-	
-	reset()
+	timer.start()
 	player_score += 1
 	player_score_label.text = "Player: " + str(player_score)
-	
+	reset()
 
 func _on_area_right_body_entered(body: Node2D) -> void:
-	
-	reset()
+	timer.start()
 	cpu_score += 1
 	cpu_score_label.text = "CPU: " + str(cpu_score)
-
+	reset()
 
 	
 func reset():
-	var timer = get_tree().create_timer(reset_time, false)
+	reset_fade_background.show()
 	
 	player.position.y = CENTER.y
 	ball.position = CENTER 
 	cpu.position.y = CENTER.y
 	ball.velocity = Vector2.ZERO
 	
-	# TODO fix timer
-	reset_timer_label.show()
-	reset_timer_label.text = str(timer.time_left)
-	
 	await timer.timeout
-	reset_timer_label.hide()
-
+	
+	reset_fade_background.hide()
+		
 	ball.ball_start_direction()
